a4c10e315a6d709ac18d5af3f14914487d3b3d17
SecretShangri-La <byobu@vivaldi.net>
2020-05-20 06:38:03 +0900

    Fixed

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,10 @@ fn t_or_f(x1: i64, ps1: &Vec<i64>) -> bool {
 
 fn prime(n1: i64) {                                                               
     let mut ps2 = vec![2];                                                        
-    let mut x2 = 3;                                                               
-    
-    if n1 < 1 {
-        println!("NOT FOWND THIS {} FIELD",n1);
+    let mut x2 = 3;    
+
+    if n1 <= 1 {
+        println!("NOT FOWND THIS {} FIELD",n1);        
     } else {
         while x2 <= n1 {                                                            
             if t_or_f(x2, &ps2) {                                                   
@@ -22,12 +22,16 @@ fn prime(n1: i64) {
             }                                                                       
             x2 += 2;                                                         
         }
-    }
-    println!("{:?}",&ps2);                                                    
+        println!("{:?}", &ps2);
+    }                                                  
 }                                                                                   
                                                                                     
 fn main() {                                                                         
-    let arg: String = std::env::args().nth(1).unwrap();                             
-    let p: i64 = arg.parse().unwrap();                                              
-    prime(p);                                                                       
+    let arg: String = std::env::args().nth(1).unwrap();
+ //   if arg.len() == 0 {  
+//        println!("NO INSET VALUES");
+//    } else {          
+        let p: i64 = arg.parse().unwrap();                                              
+        prime(p);     
+//    }                                                                  
 }
